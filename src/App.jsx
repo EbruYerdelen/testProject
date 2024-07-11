08ddc8d1a6d276f0eca8b63c343e03d75accb84c
Ebru <yerdebru@gmail.com>
2024-07-12 00:41:34 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="h-28 w-28 transition bg-slate-800 animate-spin rounded-full">
+        hello world
+      </h1>
+    </>
+  );
+}
+
+export default App
