c04ac41f6ea557d37467bb22882a56504a5243d1
Ebru <yerdebru@gmail.com>
2024-07-18 13:14:11 +0300

    button removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,10 +6,7 @@ function App() {
 
   return (
     <>
-      <h1 className="h-28 w-28 transition bg-slate-800 animate-spin rounded-full">
-        hello world
-      </h1>
-      <button className='rounded-xl bg-yellow-800 md:h-24 w-64' type='submit'>Submit</button>
+      
     </>
   );
 }
